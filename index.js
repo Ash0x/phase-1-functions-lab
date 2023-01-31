0fbd82602c243300cdba1f78ced18b09447d3f9d
Alexander <alex.galev@yahoo.com>
2023-01-31 09:12:41 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (blocks) => {
+    if (blocks > 42) return blocks - 42
+    else return 42 - blocks
+}
+const distanceFromHqInFeet = (blocks) => {
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+const distanceTravelledInFeet = (startBlock, endBlock) => {
+    return Math.abs(startBlock - endBlock) * 264
+}
+
+const calculatesFarePrice = (start, destination) => {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance > 2500) return 'cannot travel that far'
+    else if (distance <= 2500 && distance > 2000) return 25
+    else if (distance <= 2000 && distance >= 400) return (distance - 400) * 0.02
+    else return 0
+}
